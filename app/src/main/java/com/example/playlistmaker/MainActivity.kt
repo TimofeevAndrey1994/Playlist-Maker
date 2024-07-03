827e1d569f828a428f0d403e5462bcfcc8d0d1d2
atimofeev <atimofeev@rnd.dkb61>
2024-07-03 15:35:06 +0300

    домашние задания 8 спринта на проверку

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,20 +1,37 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        buttonSearch.setOnClickListener{
+            goToActivity(SearchActivity::class.java)
         }
+
+        val buttonMediaLibrary = findViewById<Button>(R.id.button_media_library)
+        val onClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                goToActivity(MediaLibraryActivity::class.java)
+            }
+        }
+        buttonMediaLibrary.setOnClickListener(onClickListener)
+
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
+        buttonSettings.setOnClickListener{
+            goToActivity(SettingsActivity::class.java)
+        }
+    }
+
+    private fun goToActivity(cls: Class<*>){
+        val intent = Intent(this, cls)
+        startActivity(intent)
     }
 }
